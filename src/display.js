19588b5cabe41811bf9658c5e3b47e5bb182feb4
crL96 <190506813+crL96@users.noreply.github.com>
2025-03-05 15:37:23 +0100

    Add renderBoard function

---
+++ src/display.js
@@ -0,0 +1,26 @@
+import "./style.css"
+
+function renderBoard(board, UIboardNr) {
+    const boardElement = document.querySelector(`#board${UIboardNr}`);
+    boardElement.replaceChildren("");
+    for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+            const squareElement = document.createElement("div");
+            squareElement.dataset.x = [j];
+            squareElement.dataset.y = [i];
+            if (board[j][i] === undefined) {
+                squareElement.classList.add("unexplored");
+            } else if (board[j][i] === "hit") {
+                squareElement.classList.add("hit");
+            } else if (board[j][i] === "miss") {
+                squareElement.classList.add("miss");
+            } else {
+                squareElement.classList.add("ship");
+            }
+            boardElement.appendChild(squareElement);
+        }
+    }
+
+}
+
+export {renderBoard};
