babfefbb44cb7f175f86e7285b5c03f26d83d46a
crL96 <190506813+crL96@users.noreply.github.com>
2025-03-06 11:56:07 +0100

    Add btn function for rng ship placement and stop user from attacking before placing ships

--- src/userInput.js
+++ src/userInput.js
@@ -5,12 +5,15 @@ import {
     computerAttacks,
     humanAttacks,
     checkGameWon,
-    newGame
+    newGame,
+    randomPlaceShip,
 } from "./game.js";
 
 //Event listeners
 const boardElement = document.querySelector("#board2");
 boardElement.addEventListener("click", (e) => {
+    if (humanPlayer.gameboard.allSunk() === true) return;
+
     const x = e.target.getAttribute("data-x");
     const y = e.target.getAttribute("data-y");
     if (x == undefined || y == undefined) return;
@@ -41,6 +44,17 @@ const newGameBtn = document.querySelector("#newGameBtn");
 newGameBtn.addEventListener("click", () => {
     newGame();
     renderBoard(enemyBoardVisible, 2);
+    renderBoard(humanPlayer.gameboard.board, 1);
+});
+
+const randomPlaceBtn = document.querySelector("#randomPlaceBtn");
+randomPlaceBtn.addEventListener("click", () => {
+    newGame();
+    randomPlaceShip(5, humanPlayer);
+    randomPlaceShip(4, humanPlayer);
+    randomPlaceShip(3, humanPlayer);
+    randomPlaceShip(3, humanPlayer);
+    randomPlaceShip(2, humanPlayer);
     renderBoard(humanPlayer.gameboard.board, 1);
 });
 
